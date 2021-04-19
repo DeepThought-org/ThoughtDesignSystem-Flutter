ba5d9325c0f53f736113fa75774ee8dc50645535
kimdohun <kimdohun0104@gmail.com>
2021-04-19 22:51:10 +0900

    [MOVE] 디자인시스템 파일 구조 변경

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:thought_design_system/component/textField/thought_text_field.dart';
 import 'package:thought_design_system/component/toolbar/thought_toolbar.dart';
 import 'package:thought_design_system/component/toolbar/thought_toolbar_style.dart';
 import 'package:thought_design_system/element/color.dart';
-import 'package:thought_design_system/element/icon.dart';
+import 'package:thought_design_system/element/spacing.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
             primarySwatch: Colors.blue,
-            scaffoldBackgroundColor: ThoughtColor.Background),
+            scaffoldBackgroundColor: ThoughtColor.Background
+        ),
         home: Scaffold(
           appBar: EmptyToolbar(),
           body: Column(
@@ -24,6 +26,15 @@ class MyApp extends StatelessWidget {
                 title: "완전 중요한 헤더\n입니다",
                 style: ThoughtToolbarStyle.Big,
               ),
+              ThoughtTextField(
+                hint: "이름",
+                onChanged: (text) {},
+              ),
+              ThoughtSpacing.Large.box,
+              ThoughtTextField(
+                hint: "비밀번호",
+                onChanged: (text) {},
+              )
             ],
           ),
         ));
