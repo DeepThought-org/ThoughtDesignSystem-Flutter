19963278e4df11e73cd1a59ab4e0f67d4960c6c1
kimdohun <kimdohun0104@gmail.com>
2021-04-20 21:19:10 +0900

    [UPDATE] Thought typography to TextTheme

--- lib/element/typography.dart
+++ lib/element/typography.dart
@@ -1,54 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:thought_design_system/element/color.dart';
 
-class ThoughtTypography {
-  static const H1 = const TextStyle(
+const LightWeight = FontWeight.w300;
+const MediumWeight = FontWeight.w500;
+
+const ThoughtTextTheme = const TextTheme(
+  headline1: TextStyle(
+    fontFamily: 'Spoqa',
     color: ThoughtColor.OnBackground,
+    fontWeight: LightWeight,
+    fontSize: 96,
+  ),
+  headline2: TextStyle(
+    fontFamily: 'Spoqa',
+    color: ThoughtColor.OnBackground,
+    fontWeight: MediumWeight,
+    fontSize: 60,
+  ),
+  headline3: TextStyle(
+    fontFamily: 'Spoqa',
+    color: ThoughtColor.OnBackground,
+    fontWeight: FontWeight.bold,
     fontSize: 48,
+  ),
+  headline4: TextStyle(
     fontFamily: 'Spoqa',
-  );
-
-  static const H2 = const TextStyle(
     color: ThoughtColor.OnBackground,
+    fontWeight: MediumWeight,
     fontSize: 34,
+  ),
+  headline5: TextStyle(
     fontFamily: 'Spoqa',
-  );
-
-  static const Sub1 = const TextStyle(
     color: ThoughtColor.OnBackground,
     fontSize: 24,
+  ),
+  headline6: TextStyle(
     fontFamily: 'Spoqa',
-  );
-
-  static const Sub2 = const TextStyle(
     color: ThoughtColor.OnBackground,
+    fontWeight: MediumWeight,
     fontSize: 20,
+  ),
+  button: TextStyle(
     fontFamily: 'Spoqa',
-  );
-
-  static const Body1 = const TextStyle(
+    color: ThoughtColor.OnBackground,
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  ),
+  bodyText1: TextStyle(
+    fontFamily: 'Spoqa',
     color: ThoughtColor.OnBackground,
     fontSize: 16,
+  ),
+  bodyText2: TextStyle(
     fontFamily: 'Spoqa',
-  );
-
-  static const Body2 = const TextStyle(
     color: ThoughtColor.OnBackground,
     fontSize: 14,
+  ),
+  caption: TextStyle(
     fontFamily: 'Spoqa',
-  );
-
-  static const Caption = const TextStyle(
     color: ThoughtColor.OnBackground,
     fontSize: 12,
+  ),
+  overline: TextStyle(
     fontFamily: 'Spoqa',
-  );
-
-  ThoughtTypography._();
-}
+    color: ThoughtColor.OnBackground,
+    fontWeight: FontWeight.bold,
+    fontSize: 10,
+  ),
+);
 
 extension ThoughtTypographyExt on TextStyle {
-  TextStyle get light => copyWith(fontWeight: FontWeight.w300);
+  TextStyle get light => copyWith(fontWeight: LightWeight);
+
+  TextStyle get medium => copyWith(fontWeight: MediumWeight);
 
   TextStyle get bold => copyWith(fontWeight: FontWeight.bold);
 }
