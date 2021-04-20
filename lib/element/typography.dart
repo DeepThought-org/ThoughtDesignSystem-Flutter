6c97625470bc5e051e9a0fa799a5c4fce55454bb
kimdohun <kimdohun0104@gmail.com>
2021-04-20 21:36:42 +0900

    [UPDATE] Thought color to ColorScheme

--- lib/element/typography.dart
+++ lib/element/typography.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:thought_design_system/element/color.dart';
 
 const LightWeight = FontWeight.w300;
 const MediumWeight = FontWeight.w500;
@@ -7,63 +6,52 @@ const MediumWeight = FontWeight.w500;
 const ThoughtTextTheme = const TextTheme(
   headline1: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontWeight: LightWeight,
     fontSize: 96,
   ),
   headline2: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontWeight: MediumWeight,
     fontSize: 60,
   ),
   headline3: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontWeight: FontWeight.bold,
     fontSize: 48,
   ),
   headline4: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontWeight: MediumWeight,
     fontSize: 34,
   ),
   headline5: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontSize: 24,
   ),
   headline6: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontWeight: MediumWeight,
     fontSize: 20,
   ),
   button: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontWeight: FontWeight.bold,
     fontSize: 18,
   ),
   bodyText1: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontSize: 16,
   ),
   bodyText2: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontSize: 14,
   ),
   caption: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontSize: 12,
   ),
   overline: TextStyle(
     fontFamily: 'Spoqa',
-    color: ThoughtColor.OnBackground,
     fontWeight: FontWeight.bold,
     fontSize: 10,
   ),
