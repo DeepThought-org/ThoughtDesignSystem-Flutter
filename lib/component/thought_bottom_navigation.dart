d52beaf2934eddabd995abf8fb8168d60c1ec5fe
kimdohun <kimdohun0104@gmail.com>
2021-04-29 21:39:40 +0900

    [UPDATE] ThoughtBottomNavigation component

---
+++ lib/component/thought_bottom_navigation.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ThoughtBottomNavigationItem {
+  final String name;
+  final Widget icon;
+  final Widget view;
+
+  ThoughtBottomNavigationItem({
+    required this.name,
+    required this.icon,
+    required this.view,
+  });
+}
+
+abstract class _ThoughtBottomNavigationStyle extends StatefulWidget {
+  const _ThoughtBottomNavigationStyle({
+    Key? key,
+    required this.items,
+  }) : super(key: key);
+
+  final List<ThoughtBottomNavigationItem> items;
+}
+
+class ThoughtBottomNavigation extends _ThoughtBottomNavigationStyle {
+  ThoughtBottomNavigation({
+    Key? key,
+    required List<ThoughtBottomNavigationItem> items,
+  }) : super(
+          key: key,
+          items: items,
+        );
+
+  @override
+  _ThoughtBottomNavigationState createState() =>
+      _ThoughtBottomNavigationState();
+}
+
+class _ThoughtBottomNavigationState extends State<ThoughtBottomNavigation> {
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(child: widget.items[_index].view),
+        _buildBottomNavigationBar(context),
+      ],
+    );
+  }
+
+  _buildBottomNavigationBar(BuildContext context) => BottomNavigationBar(
+        items: widget.items
+            .map((item) => BottomNavigationBarItem(
+                  icon: Padding(
+                    child: item.icon,
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                  ),
+                  label: item.name,
+                ))
+            .toList(),
+        currentIndex: _index,
+        onTap: _onTapItem,
+        selectedItemColor: Theme.of(context).colorScheme.primaryVariant,
+        unselectedItemColor: Colors.grey[800]!,
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+      );
+
+  _onTapItem(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+}
--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:thought_design_system/component/thought_text_field.dart';
+import 'package:thought_design_system/component/thought_bottom_navigation.dart';
+import 'package:thought_design_system/element/icon.dart';
 import 'package:thought_design_system/element/theme.dart';
 
 void main() {
@@ -11,26 +12,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ThoughtApp(
       child: Scaffold(
-        appBar: AppBar(title: Text("Hello")),
-        body: Column(
-          children: [
-            ThoughtTextField(
-              hint: "Hello",
-              onChanged: (text) {},
-              unit: "살",
-              errorText: "Hello",
-              limit: 3,
-              currentLength: 1,
-            ),
-            ThoughtTextField(
-              hint: "Hello",
-              onChanged: (text) {},
-            ),
-            ThoughtTextField.outline(
-              hint: "Hello",
-              onChanged: (string) {},
-              lines: 3,
-              limit: 1000,
+        body: ThoughtBottomNavigation(
+          items: [
+            ThoughtBottomNavigationItem(
+                name: "Hello",
+                icon: Icon(Icons.ac_unit),
+                view: Scaffold(
+                  appBar: AppBar(title: Text("Hello")),
+                  body: Container(),
+                )),
+            ThoughtBottomNavigationItem(
+              name: "Hello",
+              icon: ThoughtIcon.back,
+              view: Scaffold(
+                appBar: AppBar(title: Text("Hello12323")),
+                body: Container(),
+              ),
             ),
           ],
         ),
